79410baa206627989cd33d24c0cf9930fc11a10f
meap02 <79549780+meap02@users.noreply.github.com>
2023-05-06 22:25:39 -0700

    started clustering.py

--- clustering.py
+++ clustering.py
@@ -19,21 +19,31 @@ df = pd.read_csv('training_data.csv', sep=',', header=None) #reading the data by
 
 #assign your training data to X_training feature matrix
 
-#run kmeans testing different k values from 2 until 20 clusters
-     #Use:  kmeans = KMeans(n_clusters=k, random_state=0)
-     #      kmeans.fit(X_training)
-     #--> add your Python code
+X_training = np.array(df.values)
 
+best_k = 0
+silhouette_array = []
+for k in range(2,21):
+     kmeans = KMeans(n_clusters=k, random_state=0, n_init=10)
+     kmeans.fit(X_training)
+     #for each k, calculate the silhouette_coefficient by using: 
+     silhouette_array.append(silhouette_score(X_training, kmeans.labels_))
 
-     #for each k, calculate the silhouette_coefficient by using: silhouette_score(X_training, kmeans.labels_)
-     #find which k maximizes the silhouette_coefficient
-     #--> add your Python code here
 
 #plot the value of the silhouette_coefficient for each k value of kmeans so that we can see the best k
-#--> add your Python code here
+plt.plot(range(2,21), silhouette_array)
+plt.title("Silhouette Coefficient for each K")
+plt.xlabel("K")
+plt.ylabel("Silhouette Coefficient")
+#plt.show()
 
+best_k = 10
 #reading the test data (clusters) by using Pandas library
-#--> add your Python code here
+X_test = pd.read_csv('testing_data.csv', sep=',', header=None).values.reshape(1,-1)[0]
+kmeans = KMeans(n_clusters=k, random_state=0, n_init=10)
+kmeans.fit(X_training)
+print(X_test)
+print(kmeans.labels_)
 
 #assign your data labels to vector labels (you might need to reshape the row vector to a column vector)
 # do this: np.array(df.values).reshape(1,<number of samples>)[0]
